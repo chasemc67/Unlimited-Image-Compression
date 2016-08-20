5e149547fbcc588a7dc14860ddbf1ebd541fdc6e
Tian Zhi Wang <wtianzhi@gmail.com>
2016-08-20 11:49:48 -0600

    Node Server

---
+++ server/config/express.js
@@ -0,0 +1,56 @@
+/**
+ * Express configuration
+ */
+
+'use strict';
+
+import express from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import cookieParser from 'cookie-parser';
+import errorHandler from 'errorhandler';
+import path from 'path';
+import config from './environment';
+import session from 'express-session';
+import connectMongo from 'connect-mongo';
+import mongoose from 'mongoose';
+var mongoStore = connectMongo(session);
+
+module.exports = function(app) {
+  var env = app.get('env');
+
+  app.set('views', config.root + '/server/views');
+  app.set('view engine', 'html');
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+  
+
+  app.use(session({
+    secret: config.secrets.session,
+    saveUninitialized: true,
+    resave: false,
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection,
+      db: 'uic'
+    })
+  }));
+
+  app.set('appPath', path.join(config.root, 'client'));
+
+  if ('production' === env) {
+    app.use(express.static(app.get('appPath')));
+    app.use(morgan('dev'));
+  }
+
+  if ('development' === env || 'test' === env) {
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(app.get('appPath')));
+    app.use(morgan('dev'));
+    app.use(errorHandler()); // Error handler - has to be last
+  }
+};
