7a986ddc4e2c96c8742a99cb24234ffe79e2e262
Kyle Carlstrom <kylecarlstrom@Kyles-MacBook-Pro.local>
2016-08-20 13:48:54 -0600

    Remove redundant code

--- src/Util.js
+++ src/Util.js
@@ -10,32 +10,3 @@ export default (matrix, xLength, yLength, numXBlocks, numYBlocks) => {
     }
     return results
 }
-
-const sumBlock = (matrix, xStart, xEnd, yStart, yEnd, rowLength, yMax) => {
-    xEnd = Math.min(xEnd, rowLength);
-    yEnd = Math.min(yEnd, yMax);
-    const result = {red: 0, green: 0, blue: 0, alpha: 0};
-    let count = 0;
-    for (let y = yStart; y < yEnd; y += rowLength) {
-        for (let x = xStart; x < xEnd; x += 1) {
-            const pixel = getPixel(matrix, x, y, rowLength);
-            console.log(x, y)
-            // console.log(pixel)
-            result.red += pixel.red;
-            result.green += pixel.green;
-            result.blue += pixel.blue;
-            result.alpha += pixel.alpha;
-            count += 1;
-        }
-    }
-    result.red = result.red / count;
-    result.green = result.green / count;
-    result.blue = result.blue / count;
-    result.alpha = result.alpha / count;
-    return result
-}
-
-const getPixel = (matrix, x, y, rowLength) => {
-    const index = (y * rowLength + x) * 4
-    return {red: matrix[index], green: matrix[index + 1], blue: matrix[index + 2], alpha: matrix[index + 3]};
-}
