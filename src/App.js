6958ca5cac3a8e9fc405af4b8dd31e156e1e995d
chasemc67 <chasemc67@gmail.com>
2016-08-20 10:34:25 -0600

    Created landing page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ export default class App extends Component {
   render() {
     return (
     	<div className="root">
-      		<h1>Hello, world.</h1>
-      	</div>
+      		<div className="title">
+      			Excellent Image Compressor
+      		</div>
+
+      		<div className="about">
+      			<b>Quickly</b> and <b>securely</b> compress <b id="emphasize">GIGABYTES</b> of photos down to bytes or less. <br />
+      			With our revolutionary one-way compression algorithms, you get state of the art security while keeping disk space to an absolute minimum. <br />
+      			Safely share pictures with your friends, without worying about state sponsored hackers or foreign nationalists.
+      			<br />
+      			<br />
+      			Upload your image in the box below, select how much you want to compress your image, and watch the magic happen.
+      			In one click, either download your newly compressed image, or upload to our site to share with friends.
+      		</div>
+
+      		<div className="uploaderContainer">
+      			<div className="uploaderInput"></div>
+      			<div className="uploaderOutput"></div>
+      		</div>
+     	</div>
     );
   }
 }
